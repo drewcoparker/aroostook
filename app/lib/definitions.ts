2eceb7c8c6ccd542c58c98f60203ad2009c276e2
drewcoparker <drew.i.parker.com>
2024-08-08 15:19:17 -0400

    Inclusion of prisma client and postgres db

---
+++ app/lib/definitions.ts
@@ -0,0 +1,23 @@
+export type CongressMember = {
+  address: string;
+  bioguideid: string;
+  birthyear: string;
+  chamber: string;
+  cosponsorelegislationcount: number;
+  currentember: boolean;
+  directordername: string;
+  district: number;
+  firstname: string;
+  honorificname: string;
+  imageurl: string;
+  invertedordername: string;
+  lastname: string;
+  middlename: string;
+  officialwebsiteurl: string;
+  partyabbreviation: string;
+  partyname: string;
+  phonenumber: string;
+  sponsoredlegislationcount: number;
+  state: string;
+  termcount: number;
+};
